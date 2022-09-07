706dc25315deed8325d54ef6e8bf51bdb7305e0a
Victoria Gerasimova <zzazzky@gmail.com>
2022-09-07 22:32:53 +0500

    fix: устранена ошибка определения переменной cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const cards = [
+  {
+    name: 'Балтийское море',
+    link: './images/place__balt-more.jpg',
+  },
+  {
+    name: 'Домбай',
+    link: './images/place__dombay.jpg',
+  },
+  {
+    name: 'Эльбрус',
+    link: './images/place__elbrus.jpg',
+  },
+  {
+    name: 'Камчатский край',
+    link: './images/place__kamchatka.jpg',
+  },
+  {
+    name: 'Карачаево-Черкессия',
+    link: './images/place__karachevsk.jpg',
+  },
+  {
+    name: 'Сахалин',
+    link: './images/place__sachalin.jpg',
+  },
+];
+
 const placesContainer = document.querySelector('.places__list');
 
 const popupImage = document.querySelector('.popup_type_picture');
